2d95fa19ab35a05562343ab906037a1a3e25bfba
José Esteves <joseesteves199930@gmail.com>
2023-10-16 19:52:26 -0400

    feat: :sparkles: cors
    cors globales activadas
    
    #	modified:   package.json #	modified:   pnpm-lock.yaml #	modified:   src/config/express.js

--- src/config/express.js
+++ src/config/express.js
@@ -1,9 +1,11 @@
 import rolRoutes from '#Routes/rol.routes.js';
 import userRoutes from '#Routes/user.routes.js';
+import cors from 'cors';
 import express from 'express';
 const expressApp = express();
 // middleware
 expressApp.use(express.json());
+expressApp.use(cors());
 // routes
 expressApp.use('/user', userRoutes);
 expressApp.use('/rol', rolRoutes);
