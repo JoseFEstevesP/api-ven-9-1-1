e3728d6ffa317018ad1a588600b8e5033fed375c
José Esteves <joseesteves199930@gmail.com>
2023-11-06 16:20:36 -0400

    refactor: :recycle: validación de relacion
    se valido la relación de la tabla rol y site
    
    #	modified:   src/controllers/rol-delete.controller.js #	modified:   src/controllers/site-delete.controller.js

--- src/controllers/rol-delete.controller.js
+++ src/controllers/rol-delete.controller.js
@@ -1,9 +1,15 @@
 import { Rol } from '#Schemas/rol.schema.js';
+import { User } from '#Schemas/user.schema.js';
 const rolDeleteController = async (req, res) => {
   const { uid } = req.body;
   const existingRolById = await Rol.findByPk(uid);
   if (!existingRolById)
     return res.status(401).send({ errors: [{ uid: 'Rol no encontrado' }] });
+  const userRelation = await User.findAll({ where: { uidRol: uid } });
+  if (userRelation.length > 0)
+    return res
+      .status(401)
+      .send({ errors: [{ uid: 'Rol vinculado a un usuario' }] });
   await existingRolById.destroy();
   return res.send({ msg: 'Rol eliminado' });
 };
--- src/controllers/site-delete.controller.js
+++ src/controllers/site-delete.controller.js
@@ -1,9 +1,15 @@
 import { Site } from '#Schemas/site.schema.js';
+import { User } from '#Schemas/user.schema.js';
 const siteDeleteController = async (req, res) => {
   const { uid } = req.body;
   const existingSiteById = await Site.findByPk(uid);
   if (!existingSiteById)
     return res.status(401).send({ errors: [{ uid: 'Sede no encontrado' }] });
+  const userRelation = await User.findAll({ where: { uidSite: uid } });
+  if (userRelation.length > 0)
+    return res
+      .status(401)
+      .send({ errors: [{ uid: 'Sede vinculada a un usuario' }] });
   await existingSiteById.destroy();
   return res.send({ msg: 'Sede eliminado' });
 };
