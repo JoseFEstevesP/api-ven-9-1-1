8740901d76388972c3967a51ede2e7a13e3e25b0
José Esteves <joseesteves199930@gmail.com>
2023-11-24 13:39:58 -0400

    index

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const bootstrap = async () => {
     await sequelize
       .authenticate()
       .then(() => console.log('database connected'));
-    await sequelize.sync();
-    // await sequelize.sync({ alter: true });
+    // await sequelize.sync();
+    await sequelize.sync({ alter: true });
     httpServer.listen(process.env.PORT, () => {
       console.log(`server in port ${process.env.PORT}`);
     });
