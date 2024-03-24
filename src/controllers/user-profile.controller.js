2d42bcd3a32f9c12d6df615d1867c30ec272341a
José Esteves <joseesteves199930@gmail.com>
2024-03-24 19:47:10 -0400

    refactor: :recycle: mejora en la solicitud del perfil
    se mejoro la paticion del perfil
    
    #	modified:   src/controllers/user-profile.controller.js

--- src/controllers/user-profile.controller.js
+++ src/controllers/user-profile.controller.js
@@ -4,18 +4,38 @@ import { Site } from '#Schemas/site.schema.js';
 import { User } from '#Schemas/user.schema.js';
 const userProfileController = async (req, res) => {
   const { id } = req;
+  const status = '1';
   const existingUserById = await User.findOne({
-    where: { uid: id, status: '1' },
+    where: { uid: id, status },
+    include: [
+      {
+        model: Rol,
+        where: { status },
+        attributes: ['name'],
+      },
+      {
+        model: Site,
+        where: { status },
+        attributes: ['name'],
+      },
+    ],
+    attributes: {
+      exclude: [
+        'uidRol',
+        'uidSite',
+        'status',
+        'createAtDate',
+        'createAtTime',
+        'updateAtDate',
+        'updateAtTime',
+        'createdAt',
+        'updatedAt',
+      ],
+    },
   });
   if (!existingUserById)
     return res.status(401).send({ errors: [{ uid: userMSG.noFound }] });
-  const { uid, ci, name, surname, email, uidRol, uidSite } = existingUserById;
-  const { name: nameRol } = await Rol.findOne({
-    where: { uid: uidRol, status: '1' },
-  });
-  const { name: nameSite } = await Site.findOne({
-    where: { uid: uidSite, status: '1' },
-  });
-  return res.send({ uid, ci, name, surname, email, nameRol, nameSite });
+
+  return res.send(existingUserById);
 };
 export default userProfileController;
