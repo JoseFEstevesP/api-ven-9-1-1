2db2a09db7f26bb10c84b69011bc242e4acaa4bb
jose Esteves <joseesteves199930@gmail.com>
2025-02-03 21:18:18 -0400

    fix: :bug: corregir clave de cifrado en el middleware y controlador de inicio de sesión

--- src/middleware/user-crypto.middleware.js
+++ src/middleware/user-crypto.middleware.js
@@ -5,7 +5,7 @@ const userCryptoDTO = async (req, res, next) => {
     return res.status(401).send({ errors: ['Usuario no autorizado'] });
   const cryptoData = crypto.AES.decrypt(
     authorization.split(' ')[1],
-    process.env.CRYPTO_KEY
+    process.env.JWT_PRIVATE_KEY
   ).toString(crypto.enc.Utf8);
   req.jwt = cryptoData;
   next();
